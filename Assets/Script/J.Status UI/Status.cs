a740794cd20907f06408ea9f495d6cf39e0fe674
DoOda11 <xorjs1183@gmail.com>
2024-08-14 11:27:33 +0900

    0814

--- Assets/Script/J.Status UI/Status.cs
+++ Assets/Script/J.Status UI/Status.cs
@@ -2,25 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class Status : MonoBehaviour
 {
-    public TextMeshProUGUI text;
-    public int gold;
-
-    void Start()
-    {
-        gold = 0;
-    }
+    public Text HpText; //체력
+    public Text GoldText; //골드
+    public Text KillText; //킬수
+    public Text ManaText; // 마나
+    
+    private int hp = 100;
+    private int gold = 0;
+    private int kill = 0;
+    private int mana = 100;
 
     void Update()
     {
-        text.text = gold;
+        //텍스트로 표시
+        HpText.text = "HP: " + hp.ToString();
+        GoldText.text = "Gold: " + gold.ToString();
+        KillText.text = "Kill: " + kill.ToString();
+        ManaText.text = "Mana: " + mana.ToString();
 
+        //스페이스바 누르면 변화
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            hp -= 1;
             gold += 1;
+            kill += 1;
+            mana -= 1;
         }
     }
 }
