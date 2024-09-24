0240aafd83a835fdec5051aaf861b8568315606a
liilllilili <chojy0510@gmail.com>
2024-09-25 00:08:53 +0900

    대쉬 버그 고침

--- Assets/Script/PlayerState/PlayerDashState.cs
+++ Assets/Script/PlayerState/PlayerDashState.cs
@@ -92,24 +92,18 @@ public class PlayerDashState : MonoBehaviour, IState<PlayerController>
     {
         Vector3 mousePosition = _playerController.CheckGround(Input.mousePosition);
         Vector3 dashDestDir = (mousePosition - transform.position).normalized;
-
-        Physics.Raycast(transform.position, dashDestDir, out dashHit, _playerController.dashPower);
-        if(dashHit.collider == null)
+        if (mousePosition == null)
+            return;
+        Physics.Raycast(transform.position, dashDestDir, out dashHit, _playerController.dashPower, 1 << LayerMask.NameToLayer("Wall"));
+        if(dashHit.collider != null)
         {
-            anim.SetBool("Dash", false);
+            _playerController.dashPower = dashHit.distance - 0.35f;    //벽 거리만큼 대쉬 거리 줄임
+            _playerController.dashSpeed = dashHit.distance + _playerController.dashSpeed;
         }
         else
         {
-            if (dashHit.collider.CompareTag("Wall"))
-            {
-                _playerController.dashPower = dashHit.distance - 0.35f;    //벽 거리만큼 대쉬 거리 줄임
-                _playerController.dashSpeed = dashHit.distance + _playerController.dashSpeed;
-            }
-            else
-            {
-                _playerController.dashPower = _playerController.dashPowerOrigin;
-                _playerController.dashSpeed = _playerController.dashSpeedOrigin;
-            }
+            _playerController.dashPower = _playerController.dashPowerOrigin;
+            _playerController.dashSpeed = _playerController.dashSpeedOrigin;
         }
         dashDest = transform.position + dashDestDir * _playerController.dashPower;
         curPosition = transform.position;
