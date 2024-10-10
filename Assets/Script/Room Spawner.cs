633ca899a2bff1d02287b033bbdabb3b1f58bb42
DoOda11 <xorjs1183@gmail.com>
2024-10-10 20:42:40 +0900

    랜덤맵 생성
    간단하게만 제작함

--- Assets/Script/Room Spawner.cs
+++ Assets/Script/Room Spawner.cs
@@ -5,4 +5,52 @@ using UnityEngine;
 public class RoomSpawner : MonoBehaviour
 {
     public int openingDirection;
+    // 1 --> 아래쪽 통로 필요
+    // 2 --> 왼쪽 통로 필요
+    // 3 --> 위쪽 통로 필요
+    // 4 --> 오른쪽 통로 필요
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+    private void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn",0.1f);
+    }
+
+    private void Spawn()
+    {
+        if (spawned == false)
+        {
+            if (openingDirection == 1)      // 아래쪽 통로 필요
+            {
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 2) // 왼쪽 통로 필요
+            {
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 3) // 위쪽 통로 필요
+            {
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 4) // 오른쪽 통로 필요
+            {
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Gruond Spawn") && other.GetComponent<RoomSpawner>().spawned == true)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
